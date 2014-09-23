f1b295e4c3e759ea5cc8783e5f13db5c6e68cc99
Dmitry Yakubov <dmitry.yakubov@gmail.com>
2014-09-23 18:55:32 +0600

    rename method checkNeighbor, delete comments, rewrite checkWinner-method

--- TicTacToe/GridModel.cs
+++ TicTacToe/GridModel.cs
@@ -43,7 +43,7 @@ namespace TicTacToe
                 
             cell.setState(type);
             countOfEmptyCells--;
-            this.winner = checkNeighbor(row, col);
+            this.winner = checkWinner1(row, col);
             changeCurrentPlayer();
             return true;
             // TODO оповестить подписчиков об изменении модели
@@ -53,7 +53,8 @@ namespace TicTacToe
 
         void changeCurrentPlayer()
         {
-            switch(this.currentPlayer){
+            switch(this.currentPlayer)
+            {
                 case ECellType.X: 
                     this.currentPlayer = ECellType.O; 
                     break;
@@ -63,193 +64,182 @@ namespace TicTacToe
             }
         }
 
-        //bool checkWin(ECellType player)
-        //{
-        //    /*******************
-        //     * надо составить массив ходов игрока
-        //     * к каждому ходу проверить соседние ячейки
-        //     * если есть, то для уже найденной ячейки проверяем наличие следующей по линии ячейки
-        //     * и так до момента определения выигрыша.
-        //     * // более удобный вариант: сразу после хода можно проверять ячейки, соседствующие с только что заполненной, чтобы не обходить все поле в поисках победной комбинации
-        //    *******************/
-        //    for(int i=0; i<this.Grid.Count; i++){
-        //        for(int j=1; j<this.Grid[i].Count; j++){
-        //            if(this.Grid[i][j].getState() == player){
-        //                // TODO описать проверку соседних ячеек, оносительно this.Grid[i][j]
-        //            }
-        //        }
-        //    }
-        //}
-
-        ECellType checkNeighbor(int row, int col)
+        ECellType checkWinner(int row, int col)
         {
-            //if (addressCell.Length != 2)
-            //{
-            //    // передать должны уже нормально спарсенный и проверенный массив, но эта проверка на всякий случай
-            //    throw.ArgumentException("Передан массив неверной размерности. массив должен состоять из 2х элементов.");
-            //}
+            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            //ПЕРЕДЕЛАТЬ ЭТОТ УЖАСНЫЙ КОД
+            // VERY VERY BAD CODE, need rewrite
+
             Cell currentCell = this.Grid[ row ][ col ];
 
+            // горизонтально
+            List<List<Cell>> winningCombination = new List<List<Cell>>();
+            List<Cell> temp;
+            
+            // вертикальные комбинации
+            if ((row - 1 >= 0) && (row + 1 < this.Grid.Count))
+            {
+                temp = new List<Cell>(3);
+                temp.Add(this.Grid[row][col]);
+                temp.Add(this.Grid[row - 1][col]);
+                temp.Add(this.Grid[row + 1][col]);
+                winningCombination.Add(temp);
+            }
+            if ((row -1 >=0) && (row - 1 - 1 >=0))
+            {
+                temp = new List<Cell>(3);
+                temp.Add(this.Grid[row][col]);
+                temp.Add(this.Grid[row-1][col]);
+                temp.Add(this.Grid[row-1-1][col]);
+                winningCombination.Add(temp);
+            }
+            if ((row +1 < this.Grid.Count) && (row + 1 + 1 < this.Grid.Count))
+            {
+                temp = new List<Cell>(3);
+                temp.Add(this.Grid[row][col]);
+                temp.Add(this.Grid[row + 1][col]);
+                temp.Add(this.Grid[row + 1 + 1][col]);
+                winningCombination.Add(temp);
+            }
+            // горизонтальные комбинации
+            if ((col - 1 >= 0) && (col + 1 < this.Grid.Count))
+            {
+                temp = new List<Cell>(3);
+                temp.Add(this.Grid[row][col]);
+                temp.Add(this.Grid[row][col - 1]);
+                temp.Add(this.Grid[row][col + 1]);
+                winningCombination.Add(temp);
+            }
+            if ((col - 1 >=0) && (col - 1 - 1 >=0))
+            {
+                temp = new List<Cell>(3);
+                temp.Add(this.Grid[row][col]);
+                temp.Add(this.Grid[row][col - 1]);
+                temp.Add(this.Grid[row][col - 1 - 1]);
+                winningCombination.Add(temp);
+            }
+            if ((col + 1 < this.Grid.Count) && (col +1 + 1 < this.Grid.Count))
+            {
+                temp = new List<Cell>(3);
+                temp.Add(this.Grid[row][col]);
+                temp.Add(this.Grid[row][col + 1]);
+                temp.Add(this.Grid[row][col + 1 + 1]);
+                winningCombination.Add(temp);
+            }
+            // комбинации левой диагонали
+            if ((row - 1 >= 0) && (col - 1 >= 0) && (row + 1 < this.Grid.Count) && (col + 1 < this.Grid.Count))
+            {
+                temp = new List<Cell>(3);
+                temp.Add(this.Grid[row][col]);
+                temp.Add(this.Grid[row - 1][col - 1]);
+                temp.Add(this.Grid[row + 1][col + 1]);
+                winningCombination.Add(temp);
+            }
+            if ((row - 1 >= 0) && (col - 1 >=0) && (row - 1 - 1 >=0) && (col - 1 - 1 >=0))
+            {
+                temp = new List<Cell>(3);
+                temp.Add(this.Grid[row][col]);
+                temp.Add(this.Grid[row - 1][col - 1]);
+                temp.Add(this.Grid[row - 1 - 1][col - 1 - 1]);
+                winningCombination.Add(temp);
+            }
+            if ((row + 1 < this.Grid.Count) && (col + 1 < this.Grid.Count) && (row + 1 + 1 < this.Grid.Count) && (col + 1 + 1 < this.Grid.Count))
+            {
+                temp = new List<Cell>(3);
+                temp.Add(this.Grid[row][col]);
+                temp.Add(this.Grid[row + 1][col + 1]);
+                temp.Add(this.Grid[row + 1 + 1][col + 1 + 1]);
+                winningCombination.Add(temp);
+            }
+            // комбинации правой диагонали
+            if ((row - 1 >= 0) && (col + 1 < this.Grid.Count) && (row + 1 < this.Grid.Count) && (col - 1 >= 0))
+            {
+                temp = new List<Cell>(3);
+                temp.Add(this.Grid[row][col]);
+                temp.Add(this.Grid[row - 1][col + 1]);
+                temp.Add(this.Grid[row + 1][col - 1]);
+                winningCombination.Add(temp);
+            }
+            if ((row - 1 >= 0) && (col + 1 < this.Grid.Count) && (row-1-1 >= 0) && (col+1+1 < this.Grid.Count))
+            {
+                temp = new List<Cell>(3);
+                temp.Add(this.Grid[row][col]);
+                temp.Add(this.Grid[row - 1][col + 1]);
+                temp.Add(this.Grid[row - 1 - 1][col + 1 + 1]);
+                winningCombination.Add(temp);
+            }
+            if ((row + 1 < this.Grid.Count) && (col - 1 >= 0) && (row + 1 + 1 < this.Grid.Count) && (col - 1 - 1 >= 0))
+            {
+                temp = new List<Cell>(3);
+                temp.Add(this.Grid[row][col]);
+                temp.Add(this.Grid[row + 1][col - 1]);
+                temp.Add(this.Grid[row + 1 + 1][col - 1 - 1]);
+                winningCombination.Add(temp);
+            }
 
-                    /*
-                     * в данный момент я хочу пройти от выбранной ячейки вверх, 
-                     *                                                   вниз, 
-                     *                                                   влево, 
-                     *                                                   вправо, 
-                     *                                                   по одной диагонали вверх, 
-                     *                                                   по одной диагонали вниз, 
-                     *                                                   по другой диагонали вверх, 
-                     *                                                   по другой диагонали вниз.
-                     */
-
-
-                     /* Возможно, стоит определить выигрышные комбинации для текущей ячейки и уже их проверять.
-                     * Таким образом исключим заведомо невыигрышные диагональные комбинации
-                     */
-                    // горизонтально
-                    List<List<Cell>> winningCombination = new List<List<Cell>>();
-                    
-                        //ПЕРЕДЕЛАТЬ ЭТОТ УЖАСНЫЙ КОД
-                        // VERY VERY BAD CODE, need rewrite
-
-                    
-                                List<Cell> temp; // если для выигрыша достаточно линии из трех одинаковых ячеек.
-                    // вертикальные комбинации
-                                if ((row - 1 >= 0) && (row + 1 < this.Grid.Count))
-                                {
-                                    temp = new List<Cell>(3);
-                                    temp.Add(this.Grid[row][col]);
-                                    temp.Add(this.Grid[row - 1][col]);
-                                    temp.Add(this.Grid[row + 1][col]);
-                                    winningCombination.Add(temp);
-                                }
-                                if ((row -1 >=0) && (row - 1 - 1 >=0))
-                                {
-                                    temp = new List<Cell>(3);
-                                    temp.Add(this.Grid[row][col]);
-                                    temp.Add(this.Grid[row-1][col]);
-                                    temp.Add(this.Grid[row-1-1][col]);
-                                    winningCombination.Add(temp);
-                                }
-                                if ((row +1 < this.Grid.Count) && (row + 1 + 1 < this.Grid.Count))
-                                {
-                                    temp = new List<Cell>(3);
-                                    temp.Add(this.Grid[row][col]);
-                                    temp.Add(this.Grid[row + 1][col]);
-                                    temp.Add(this.Grid[row + 1 + 1][col]);
-                                    winningCombination.Add(temp);
-                                }
-                    // горизонтальные комбинации
-                                if ((col - 1 >= 0) && (col + 1 < this.Grid.Count))
-                                {
-                                    temp = new List<Cell>(3);
-                                    temp.Add(this.Grid[row][col]);
-                                    temp.Add(this.Grid[row][col - 1]);
-                                    temp.Add(this.Grid[row][col + 1]);
-                                    winningCombination.Add(temp);
-                                }
-                                if ((col - 1 >=0) && (col - 1 - 1 >=0))
-                                {
-                                    temp = new List<Cell>(3);
-                                    temp.Add(this.Grid[row][col]);
-                                    temp.Add(this.Grid[row][col - 1]);
-                                    temp.Add(this.Grid[row][col - 1 - 1]);
-                                    winningCombination.Add(temp);
-                                }
-                                if ((col + 1 < this.Grid.Count) && (col +1 + 1 < this.Grid.Count))
-                                {
-                                    temp = new List<Cell>(3);
-                                    temp.Add(this.Grid[row][col]);
-                                    temp.Add(this.Grid[row][col + 1]);
-                                    temp.Add(this.Grid[row][col + 1 + 1]);
-                                    winningCombination.Add(temp);
-                                }
-                    // комбинации левой диагонали
-                                if ((row - 1 >= 0) && (col - 1 >= 0) && (row + 1 < this.Grid.Count) && (col + 1 < this.Grid.Count))
-                                {
-                                    temp = new List<Cell>(3);
-                                    temp.Add(this.Grid[row][col]);
-                                    temp.Add(this.Grid[row - 1][col - 1]);
-                                    temp.Add(this.Grid[row + 1][col + 1]);
-                                    winningCombination.Add(temp);
-                                }
-                                if ((row - 1 >= 0) && (col - 1 >=0) && (row - 1 - 1 >=0) && (col - 1 - 1 >=0))
-                                {
-                                    temp = new List<Cell>(3);
-                                    temp.Add(this.Grid[row][col]);
-                                    temp.Add(this.Grid[row - 1][col - 1]);
-                                    temp.Add(this.Grid[row - 1 - 1][col - 1 - 1]);
-                                    winningCombination.Add(temp);
-                                }
-                                if ((row + 1 < this.Grid.Count) && (col + 1 < this.Grid.Count) && (row + 1 + 1 < this.Grid.Count) && (col + 1 + 1 < this.Grid.Count))
-                                {
-                                    temp = new List<Cell>(3);
-                                    temp.Add(this.Grid[row][col]);
-                                    temp.Add(this.Grid[row + 1][col + 1]);
-                                    temp.Add(this.Grid[row + 1 + 1][col + 1 + 1]);
-                                    winningCombination.Add(temp);
-                                }
-                    // комбинации правой диагонали
-                                if ((row - 1 >= 0) && (col + 1 < this.Grid.Count) && (row + 1 < this.Grid.Count) && (col - 1 >= 0))
-                                {
-                                    temp = new List<Cell>(3);
-                                    temp.Add(this.Grid[row][col]);
-                                    temp.Add(this.Grid[row - 1][col + 1]);
-                                    temp.Add(this.Grid[row + 1][col - 1]);
-                                    winningCombination.Add(temp);
-                                }
-                                if ((row - 1 >= 0) && (col + 1 < this.Grid.Count) && (row-1-1 >= 0) && (col+1+1 < this.Grid.Count))
-                                {
-                                    temp = new List<Cell>(3);
-                                    temp.Add(this.Grid[row][col]);
-                                    temp.Add(this.Grid[row - 1][col + 1]);
-                                    temp.Add(this.Grid[row - 1 - 1][col + 1 + 1]);
-                                    winningCombination.Add(temp);
-                                }
-                                
-                                if ((row + 1 < this.Grid.Count) && (col - 1 >= 0) && (row + 1 + 1 < this.Grid.Count) && (col - 1 - 1 >= 0))
-                                {
-                                    temp = new List<Cell>(3);
-                                    temp.Add(this.Grid[row][col]);
-                                    temp.Add(this.Grid[row + 1][col - 1]);
-                                    temp.Add(this.Grid[row + 1 + 1][col - 1 - 1]);
-                                    winningCombination.Add(temp);
-                                }
-
-                    for (int i=0; i<winningCombination.Count; i++){
-                        
-                        if (winningCombination[i][0].getState()==currentCell.getState() &&
-                            winningCombination[i][1].getState()==currentCell.getState() &&
-                            winningCombination[i][2].getState()==currentCell.getState())
-                        {
-                            //Console.WriteLine("{0} win", currentCell.ToString());
-                            return currentCell.getState();
-                        }
-                            
-                    }
-                    return ECellType.Empty;
-
-
-
-
-            /*
-             * получаем адрес ячейки в формате массива [строка, столбец]
-             * вертикальные совпадения:
-             * проверяем наличие ячейки снизу. (важен контроль границ поля)
-             * начало рекурсии
-             * если есть ячейка, 
-             *      проверяем наличие ячейки снизу. (важен контроль границ поля)
-             *      счетчик +1.
-             * конец рекурсии
-             * если нет ячейки, 
-             *      проверяем наличие ячейки сверху от первоначальной. (важен контроль границ поля)
-             * начало рекурсии
-             * если есть ячейка, 
-             *      проверяем наличие ячейки сверху. (важен контроль границ поля)
-             *      счетчик +1.
-             * конец рекурсии
-             */
+            for (int i=0; i<winningCombination.Count; i++){
+                if (winningCombination[i][0].getState()==currentCell.getState() &&
+                    winningCombination[i][1].getState()==currentCell.getState() &&
+                    winningCombination[i][2].getState()==currentCell.getState())
+                {
+                    //Console.WriteLine("{0} win", currentCell.ToString());
+                    return currentCell.getState();
+                }
+            }
+            return ECellType.Empty;
         }
 
+        ECellType checkWinner1(int row, int col)
+        {
+            if (!check(row, col, row + 1, col, 2))
+                if (!check(row, col, row - 1, col, 2))
+                    if (!check(row, col, row, col + 1, 2))
+                        if (!check(row, col, row, col - 1, 2))
+                            if (!check(row, col, row + 1, col + 1, 2))
+                                if (!check(row, col, row - 1, col - 1, 2))
+                                    if (!check(row, col, row + 1, col - 1, 2))
+                                        if (!check(row, col, row - 1, col + 1, 2))
+                                            if (!checkForCurrentCellInTheMiddle(row, col))
+                                                return ECellType.Empty;
+            return currentPlayer;
+        }
+        //bool check(int[] CurrentCoordinates, int[] TargetCoordinates, int deep)
+        bool check(int currentRow, int currentCol, int targetRow, int targetCol, int deep)
+        {
+            int sizeOfSide = this.Grid.Count;
+            bool result = false;
+            deep--;
+
+            if (targetRow >= 0 && targetRow < sizeOfSide) // проверка границ сверху/снизу
+                if (targetCol >= 0 && targetCol < sizeOfSide) // проверка границ слева/справа
+                    if (this.Grid[currentRow][currentCol].getState() == this.Grid[targetRow][targetCol].getState())
+                    {
+                        int differenceRow = targetRow - currentRow;
+                        int differenceCol = targetCol - currentCol;
+                        if (deep == 0) result = true;
+                        else result = check(targetRow, targetCol, targetRow + differenceRow, targetCol + differenceCol, deep);
+                    }
+            return result;
+        }
+        bool checkForCurrentCellInTheMiddle(int currentRow, int currentCol)
+        {
+            // вертикально
+            if (check(currentRow, currentCol, currentRow + 1, currentCol, 1))
+                if (check(currentRow, currentCol, currentRow - 1, currentCol, 1))
+                    return true;
+            // горизонтально
+            if (check(currentRow, currentCol, currentRow, currentCol + 1, 1))
+                if (check(currentRow, currentCol, currentRow, currentCol - 1, 1))
+                    return true;
+            // правая диагональ
+            if (check(currentRow, currentCol, currentRow + 1, currentCol + 1, 1))
+                if (check(currentRow, currentCol, currentRow - 1, currentCol - 1, 1))
+                    return true;
+            // левая диагональ
+            if (check(currentRow, currentCol, currentRow + 1, currentCol - 1, 1))
+                if (check(currentRow, currentCol, currentRow - 1, currentCol + 1, 1))
+                    return true;
+            return false;
+        }
     }
 }
